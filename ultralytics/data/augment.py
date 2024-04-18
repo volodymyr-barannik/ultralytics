858c1602bde7fc09b02ac164811b1b311094a46e
volodymyr-barannik <volodymyr.barannik@ukma.edu.ua>
2024-04-18 14:38:16 +0300

    disable random gamma

--- ultralytics/data/augment.py
+++ ultralytics/data/augment.py
@@ -887,7 +887,7 @@ class Albumentations:
                 A.ToGray(p=0.01),
                 A.CLAHE(p=0.01),
                 A.RandomBrightnessContrast(brightness_limit=(-0.4, 0.1), contrast_limit=(-0.1, 0.2), p=0.5),
-                A.RandomGamma(p=0.2),
+                #A.RandomGamma(p=0.2),
                 A.ImageCompression(quality_lower=75, p=0.1),
                 A.Normalize(),
             ]
